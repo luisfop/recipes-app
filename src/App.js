67ec558322ca7ed55547576725ca22ed147c6787
luisop <luisfop77@gmail.com>
2020-08-18 12:32:56 -0300

    Finished mvp of the project

--- src/App.js
+++ src/App.js
@@ -1,56 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { getRecipes } from "./Services/index";
-
-import Recipes from "./Components/Recipes/Recipes";
-
-import {APP_ID, APP_KEY } from './Enviroment';
-
-import "./App.css";
+import RecipeApp from './RecipeApp';
 
 const App = () => {
 
-  const [recipes, setRecipes] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const [inputData, setInputData] = useState("");
-  const [query, setQuery] = useState("Chicken");
-
-
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-  useEffect(() => {
-    async function fetchData() {
-      let response = await getRecipes(url, query);
-      setRecipes(response.slice(0,9));
-      setLoading(false);
-    }
-    fetchData();
-  }, [query]);
-  
-  const inputHandler = event => {
-    setInputData(event.target.value);
-  };
-
-  const submitSearch = event => {
-    event.preventDefault();
-    setQuery(inputData);
-  };
-
-  const disableButton = () => {
-    return inputData.length < 1;
-  }
-
   return (    
       <div className="App">
-        <h1>Recipe App</h1>
-        <div className="Form">
-          <form onSubmit={submitSearch}>
-            <input type="text" onChange={inputHandler} placeholder="Type the recipe that you're looking for..."/>
-            <button disabled={disableButton()}>Search</button>
-          </form>
-        </div>
-        <Recipes recipes={recipes} loading={loading} />        
+        <RecipeApp />
       </div>
     
   );
---
+++ src/RecipeApp.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+
+import { getRecipes } from "./Services";
+import {APP_ID, APP_KEY } from './Enviroment';
+
+import Recipes from "./Components/Recipes/Recipes";
+
+import "./RecipeApp.css";
+
+
+const App = () => {
+
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [inputData, setInputData] = useState("");
+  const [query, setQuery] = useState("Brownie");
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getRecipes(url, query);
+      setRecipes(response.slice(0,9));
+      setLoading(false);
+    }
+    fetchData();
+  }, [query]);
+  
+  const inputHandler = event => {
+    setInputData(event.target.value);
+  };
+
+  const submitSearch = event => {
+    event.preventDefault();
+    setQuery(inputData);
+  };
+
+  const disableButton = () => {
+    return inputData.length < 1;
+  }
+
+  return (    
+      <div className="Recipes_App">
+        <h1>Recipe App</h1>
+        <div className="Form">
+          <form onSubmit={submitSearch}>
+            <input type="text" onChange={inputHandler} placeholder="Type the recipe that you're looking for..."/>
+            <button disabled={disableButton()}>Search</button>
+          </form>
+        </div>
+        <Recipes recipes={recipes} loading={loading} />        
+      </div>
+    
+  );
+};
+
+export default App;
